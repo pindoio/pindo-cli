679ef41ec8d5450db16ec90b53fc8a50fe2c9dc4
remymuhire <jeanremy1806@gmail.com>
2018-12-06 09:35:58 +0200

    initial commit

---
+++ pindo_cli/http.py
@@ -0,0 +1,50 @@
+import json
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+
+class PindoClientException(Exception):
+    pass
+
+
+class Token:
+    """
+    Request a Pindo Token
+    """
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.url = 'http://188.166.168.177/users/token'
+
+    def __str__(self):
+        # request 
+        r = requests.get(
+            self.url, auth=HTTPBasicAuth(self.username, self.password))
+        if r.status_code == 200:
+            return '{}'.format(r.json()['token'])
+        raise PindoClientException('Pindo API authentication failed.')
+
+
+class Register:
+    """
+    Register a new account
+    """
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.url = 'http://188.166.168.177/users/registration'
+
+    def __str__(self):
+        payload = {
+            'username': self.username,
+            'email': self.email,
+            'password': self.password
+        }
+        
+        r = requests.post(self.url, json=payload)
+        if r.status_code == 201:
+            return ' Account successfully created'
+        raise PindoClientException('Pindo API registration failed.')
---
+++ setup.py
@@ -0,0 +1,46 @@
+"""
+pindo-cli
+--------------
+
+Pindo is a communication platform for humans and machines
+"""
+from setuptools import setup
+
+
+setup(
+    name='pindo-cli',
+    version='0.1',
+    url='http://github.com/pindo-io/pindo-cli',
+    license='MIT',
+    author='Team Pindo',
+    author_email='team@pindo.io',
+    description=('Pindo is a communication platform for humans and machines'),
+    packages=['pindo_cli'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    py_modules=['pindo_cli'],
+    install_requires=[
+        'Click==7.0',
+        'requests==2.4.2',
+        'pytest-cov==2.6.0',
+        'click-spinner==0.1.8',
+    ],
+    tests_require=[],
+    entry_points='''
+        [console_scripts]
+        pindo=pindo_cli:cli
+    ''',
+    test_suite="tests",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
