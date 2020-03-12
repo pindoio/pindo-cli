d63d80087d62fb1135982e8b631c4524c8674962
Thibault <933kss@gmail.com>
2020-03-12 20:20:41 +0200

    add version flag test

--- pindo_cli/tests/test_cli.py
+++ pindo_cli/tests/test_cli.py
@@ -1,5 +1,6 @@
 import pytest
 from click.testing import CliRunner
+from pkg_resources import get_distribution
 
 import pindo_cli
 
@@ -54,3 +55,9 @@ def test_command_(runner):
         ])
     assert result.exit_code == 0
     assert 'message' in result.output
+
+def test_pindo_cli_version_flag(runner):
+    result = runner.invoke(pindo_cli.cli, ['--version', "-v"])
+    assert result.exit_code == 0
+    assert get_distribution('pindo-cli').version in result.output
+    
