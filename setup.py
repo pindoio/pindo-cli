83d65607f7f20c4170f8a9ee3648a79a57e5bf7b
kenessajr <jeanremy1806@gmail.com>
2020-07-22 01:19:03 +0200

    version='0.1.8'

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pindo-cli',
-    version='0.1.7',
+    version='0.1.8',
     url='http://github.com/pindo-io/pindo-cli',
     license='MIT',
     author='Team Pindo',
