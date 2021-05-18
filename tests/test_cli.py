68d9dbeecc8e5edc46dda63bc8f4ea5fee54ede5
kenessajr <jeanremy1806@gmail.com>
2021-05-18 14:52:24 +0200

    refactor(replace python dependcy management tool setup.py to poetry): refactor code

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,7 @@ import pytest
 from click.testing import CliRunner
 from pkg_resources import get_distribution
 
-import pindo_cli
+import pindo_cli.cli as pindo
 
 
 @pytest.fixture(scope="module")
@@ -11,26 +11,26 @@ def runner():
 
 
 def test_pindo_cli(runner):
-    result = runner.invoke(pindo_cli.cli, ['--help'])
+    result = runner.invoke(pindo.cli, ['--help'])
     assert result.exit_code == 0
     assert 'Pindo CLI' in result.output
 
 
 def test_command_token(runner):
-    result = runner.invoke(pindo_cli.token, ['-u', 'abc', '-p', 'cbd'])
+    result = runner.invoke(pindo.token, ['-u', 'abc', '-p', 'cbd'])
     assert result.exit_code == 0
     assert 'message' in result.output
 
 
 def test_command_refresh_token(runner):
-    result = runner.invoke(pindo_cli.refresh_token, ['-u', 'abc', '-p', 'cbd'])
+    result = runner.invoke(pindo.refresh_token, ['-u', 'abc', '-p', 'cbd'])
     assert result.exit_code == 0
     assert 'message' in result.output
 
 
 def test_command_register(runner):
     result = runner.invoke(
-        pindo_cli.register,
+        pindo.register,
         ['-u', 'ken', '-e', 'remy@bar.io', '-p', 'bar'])
     assert result.exit_code == 0
     assert 'message' in result.output
@@ -38,7 +38,7 @@ def test_command_register(runner):
 
 def test_command_sms(runner):
     result = runner.invoke(
-        pindo_cli.sms, [
+        pindo.sms, [
             '--token', 'oeiroeoeioeoiroe',
             '--to', '+250785383100',
             '--text', 'Hello Pindo',
@@ -50,7 +50,7 @@ def test_command_sms(runner):
 
 def test_command_(runner):
     result = runner.invoke(
-        pindo_cli.balance, [
+        pindo.balance, [
             '--token', 'oeiroeoeioeoiroe',
         ])
     assert result.exit_code == 0
@@ -58,14 +58,14 @@ def test_command_(runner):
 
 
 def test_pindo_cli_version_flag(runner):
-    result = runner.invoke(pindo_cli.cli, ['--version', "-v"])
+    result = runner.invoke(pindo.cli, ['--version', "-v"])
     assert result.exit_code == 0
     assert get_distribution('pindo-cli').version in result.output
 
 
 def test_command_org(runner):
     result = runner.invoke(
-        pindo_cli.org, [
+        pindo.org, [
             '--token', 'oeiroeoeioeoiroe',
             '--name', 'Pindo',
             '--webhook_url', 'https://pindo.io/webhook',
